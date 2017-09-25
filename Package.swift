f873fedbde40b829c0014b9038bdd9521a857247
nixberg <k.nixberg@gmail.com>
2017-09-25 16:02:40 +0200

    SPM4 support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-sodium",
+    products: [
+        .library(
+            name: "Sodium",
+            targets: ["Sodium"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nixberg/Clibsodium", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Sodium",
+            dependencies: [],
+            path: ".",
+            exclude: ["Sodium/libsodium"],
+            sources: ["Sodium"]),
+    ]
+)
